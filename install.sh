66bbcec7dc02a893ea1293e5d8cf195814f27e5c
NotAJocke <jockerider199@gmail.com>
2023-11-21 13:59:12 +0100

    fix: install script

--- install.sh
+++ install.sh
@@ -34,13 +34,13 @@ if ! unzip -q "$temp_dir/release.zip" -d "$temp_dir"; then
 fi
 echo "Archive extracted successfully."
 
-chmod +x "$temp_dir/$repo-$os-$arch/whisper-keys"
+chmod +x "$temp_dir/$repo-$os-$arch/whisper-keys-engine"
 
 if [ ! -d "/usr/local/bin" ]; then
 	sudo mkdir -p /usr/local/bin
 fi
 
-if ! sudo mv "$temp_dir/$repo-$os-$arch/whisper-keys" /usr/local/bin/; then
+if ! sudo mv "$temp_dir/$repo-$os-$arch/whisper-keys-engine" /usr/local/bin/; then
 	echo "Failed to move the binary to /usr/local/bin."
 	exit 1
 fi
@@ -49,4 +49,3 @@ rm -rf "$temp_dir"
 
 echo
 echo "Installation completed successfully."
-
